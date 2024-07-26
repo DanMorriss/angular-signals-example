56864a719ef1cf46a80e83b2f92d5bf80f0e58d9
Dan Morriss <danielmorriss1@gmail.com>
2024-07-26 10:41:30 +0200

    using signals across your application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,34 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Product } from '../product';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
-  products = signal([
+  allProducts:Product[] = [
     {id: 1, name: 'Milk', price: 1.45},
-    {id: 2, name: 'Bread', price: 2.99},
-    {id: 3, name: 'Butter', price: 3.25}
-  ])
-
-  filterName = signal('');
-
-  filteredProducts = computed(() => {
-    return this.products().filter(
-      product => product.name
-      .toLowerCase()
-      .includes(this.filterName().toLowerCase()))
-  });
-
-  changeFilter(event: Event) {
-    let newFilterName = (event.target as HTMLInputElement).value;
-    this.filterName.set(newFilterName);
-  }
+    {id: 2, name: 'Bread', price: 3.90},
+    {id: 3, name: 'Butter', price: 2.25}
+  ]
   
 }
